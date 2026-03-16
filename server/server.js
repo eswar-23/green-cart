e430857b865acebf9d7739522bca99fdfe8d0bd6
eswar-23 <eswardas2025@gmail.com>
2026-03-16 19:02:43 +0530

    final

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ await connectDB()
 await connectCloudinary()
 
 // allow multiple origins to access backend
-const allowedOrigins =['http://localhost:5173']
+const allowedOrigins =['http://localhost:5173','https://green-cart-beryl-nine.vercel.app']
 
 app.post('/stripe', express.raw({type: 'application/json'}), stripeWebhooks)
 
