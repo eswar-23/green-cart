5bb56cf862fb962b665a57c0ec38abc1c9f8044c
eswar-23 <eswardas2025@gmail.com>
2026-03-16 18:45:15 +0530

    db changed

--- server/configs/db.js
+++ server/configs/db.js
@@ -2,10 +2,11 @@ import mongoose from "mongoose"
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/green-cart")
-    console.log("DB connected")
+    mongoose.connection.on('connected',()=> console.log("db connected")
+    )
+    await mongoose.connect(`${process.env.MONGODB_URI}/green-cart`)
   } catch (error) {
-    console.log(error)
+    console.error(error.message)
   }
 }
 
